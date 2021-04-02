05963e8a351ca30579b65ac946d7ae0096afdf94
dedane <evanskeema@gmail.com>
2021-04-03 01:16:51 +0300

    authorized-headers

--- index.js
+++ index.js
@@ -97,5 +97,6 @@ var unifiedServer = function(req,res){
 // Define the request router
 var router = {
   'ping' : handlers.ping,
-  'users' : handlers.users
+  'users' : handlers.users,
+  'tokens': handlers.tokens
 };
